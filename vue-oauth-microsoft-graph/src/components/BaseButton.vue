2bd75d0613a5eb5b93494aa9ea095f3920ae76b2
maybee <maybeesiness@gmail.com>
2022-09-20 16:16:28 +0200

    Ex 7: create BaseButton with primary color

---
+++ vue-oauth-microsoft-graph/src/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <Button @click="handleClick">{{buttonText}}</Button>
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            default: () => "Label"
+        },
+    },
+    methods: {
+        handleClick() {
+            console.log("Hello World");
+        },
+    },
+};
+</script>
--- vue-oauth-microsoft-graph/src/components/HomePage.vue
+++ vue-oauth-microsoft-graph/src/components/HomePage.vue
@@ -1,13 +1,44 @@
 <template>
     <div class="homepage">
         <h1>Here comes the content of the HomePage</h1>
+        <div>
+            <base-button class="common button1" buttonText="BaseButton with custom margin" ></base-button>
+            <base-button :disabled="true" class="common button2" buttonText="BaseButton disabled" aria-disabled="disabled"></base-button>
+        </div>
     </div>
 </template>
 
-<style>
-.homepage{
-    background-color: white;
-    position: static; 
-    font-family: "Gill Sans", sans-serif;   
+<script>
+import BaseButton from './BaseButton.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    BaseButton
+  }
 }
+</script>
+
+
+<style>
+    .homepage{
+        background-color: white;
+        position: static; 
+        font-family: "Gill Sans", sans-serif;   
+    }
+
+    .common{
+        padding: 10px;
+        border: none;
+        background-color: #2bb883;
+        border-radius: 10px;
+    }
+
+    .common:hover{
+        background-color: #8bd6b9;
+    }
+
+    .button1{
+        margin: 30px;
+    }
 </style>
